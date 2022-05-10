ab3b5aa8abe169c997826d8af1b169e73b58695f
zardfoever <87600155+ZardForever1009@users.noreply.github.com>
2022-05-10 08:00:55 +0800

    Add files via upload

---
+++ hw4-1.cpp
@@ -0,0 +1,48 @@
+// hw4-1: Graph implementation
+// environment: Notepad++ 8.2.1(64bits X64)
+// C++ type: C++ 14
+// g++ type: g++ version 6.3.0
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// combine all func together
+void graph_implementation(){
+	string action, data1, data2, edge;
+	while(cin>>action){
+		if(action=="addVertex"){
+			
+		}
+		else if(action=="deleteVertex"){
+			
+		}
+		else if(action=="addEdge"){
+			
+		}
+		else if(action=="deleteEdge"){
+			
+		}
+		else if(action=="connectedComponents"){
+			
+		}
+		else if(action=="Dijkstra"){
+			
+		}
+		else if(action=="BellmanFord"){
+			
+		}
+		else{
+			cout<<"Error: No corresponding function"<<endl; 
+		}
+	}
+	return;
+}
+
+int main(){
+	
+	graph_implementation();
+	
+	system("pause");
+	return 0;
+}
